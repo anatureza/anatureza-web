f2c57b049901999646542cb048665fc78087bf0a
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-10-27 21:36:06 -0300

    AnimalsTable data visualization fixes

--- src/components/AnimalsTable.tsx
+++ src/components/AnimalsTable.tsx
@@ -20,7 +20,8 @@ export function AnimalsTable({ animalsData, children }: IAnimalsDataProp) {
               <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                   <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    {animalsData ? (
+                    {typeof animalsData !== 'undefined' &&
+                    animalsData.length > 0 ? (
                       <table className="min-w-full divide-y divide-gray-200">
                         <thead className="bg-gray-50">
                           <tr>
@@ -55,7 +56,7 @@ export function AnimalsTable({ animalsData, children }: IAnimalsDataProp) {
                         </tbody>
                       </table>
                     ) : (
-                      <div className="min-h-screen">
+                      <div className="p-10 text-center">
                         <h1 className="text-2xl text-gray-700">
                           Nenhum animal encontrado
                         </h1>
