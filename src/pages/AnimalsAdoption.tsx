69ad2aaeff3e1099a23d86ef29677461480fe7d6
Gustavo Dantas Galote <gusgalote@outlook.com>
2021-11-13 00:21:02 -0300

    Added LoadMoreAnimals and Search animals

--- src/pages/AnimalsAdoption.tsx
+++ src/pages/AnimalsAdoption.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 
 import { AnimalCard } from '../components/AnimalCard';
 
@@ -7,15 +7,54 @@ import api from '../services/api';
 import { IAnimal } from '../types';
 
 export function AnimalsAdoption() {
-  const [animals, setAnimals] = useState<IAnimal[] | undefined>([]);
+  const [allAnimals, setAllAnimals] = useState<IAnimal[]>([]);
+
+  const [animals, setAnimals] = useState<IAnimal[]>([]);
+  const [page, setPage] = useState(0);
+  const [animalsPerPage] = useState(1);
+  const [searchValue, setSearchValue] = useState<string>('');
+
+  const filteredAnimals = !!searchValue
+    ? allAnimals.filter((animal) => {
+        return animal.name.toLowerCase().includes(searchValue.toLowerCase());
+      })
+    : animals;
+
+  const handleLoadAnimals = useCallback(async (page, animalsPerPage) => {
+    const animalsFromLoadAnimals = await loadAnimals();
+
+    setAnimals(animalsFromLoadAnimals.slice(page, animalsPerPage));
+    setAllAnimals(animalsFromLoadAnimals);
+  }, []);
+
+  const noMoreAnimals = page + animalsPerPage >= allAnimals.length;
+
+  async function loadAnimals() {
+    window.scrollTo(0, 0);
+    const { data } = await api.get<IAnimal[]>('/available-animals');
+    return data;
+  }
+
+  async function loadMoreAnimals() {
+    const nextPage = page + animalsPerPage;
+    const nextAnimals = allAnimals.slice(nextPage, nextPage + animalsPerPage);
+    animals.push(...nextAnimals);
+
+    setAnimals(animals);
+    setPage(nextPage);
+  }
+
+  function handleSearchChange(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    setSearchValue(value);
+  }
 
   useEffect(() => {
-    (async () => {
-      window.scrollTo(0, 0);
-      const { data } = await api.get<IAnimal[]>('/all-animals');
-      setAnimals(data.filter((animal) => animal.available));
-    })();
-  }, []);
+    window.scrollTo(0, 0);
+    handleLoadAnimals(0, animalsPerPage);
+  }, [handleLoadAnimals, animalsPerPage]);
+
   return (
     <>
       <div className="w-full bg-white p-12">
@@ -33,27 +72,44 @@ export function AnimalsAdoption() {
             <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
               <div className="relative md:grid-cols-4">
                 <input
-                  type="text"
+                  type="search"
                   id="form-subscribe-Search"
+                  value={searchValue}
+                  onChange={handleSearchChange}
                   className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
-                  placeholder="Pesquisar"
+                  placeholder="Pesquisar animais"
                 />
               </div>
             </form>
           </div>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
-          {typeof animals !== 'undefined' &&
-            (animals.length > 0 ? (
-              animals.map((animal) => (
+          {filteredAnimals.length > 0 && (
+            <>
+              {filteredAnimals.map((animal) => (
                 <AnimalCard key={animal.id} {...animal} />
-              ))
-            ) : (
-              <div className="container h-screen">
-                <h1>Nenhum animal disponivel para ado&#231;&#227;o</h1>
-              </div>
-            ))}
+              ))}
+            </>
+          )}
+          {filteredAnimals.length === 0 && (
+            <div className="container">
+              <h1>Nenhum animal encontrado</h1>
+            </div>
+          )}
         </div>
+        {searchValue === '' && (
+          <div className="w-full py-8 px-12">
+            <button
+              onClick={loadMoreAnimals}
+              disabled={noMoreAnimals}
+              className={`${
+                noMoreAnimals ? 'bg-gray-700' : 'bg-blue-700 hover:bg-blue-500'
+              } text-white p-4 w-full rounded-lg font-semibold`}
+            >
+              Carregar mais animais
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
